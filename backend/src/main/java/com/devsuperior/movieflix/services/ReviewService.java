ee25736d91ebe62ee9831d1ecc98a5d7b9ef074c
Agny <agnyfonseca@outlook.com>
2022-02-07 14:25:56 +0100

    Backend added

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,46 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	private ReviewRepository repository;
+	
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Autowired
+	private AuthService authService;
+
+	@Transactional
+	public ReviewDTO insert(ReviewDTO dto) {
+		User user = authService.authenticated();
+		Review entity = new Review();
+		entity.setMovie(movieRepository.getOne(dto.getMovieId()));
+		entity.setUser(user);
+		entity.setText(dto.getText());
+		repository.save(entity);
+		return new ReviewDTO(entity);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<ReviewDTO> findAll() {
+		List<Review> list = repository.findAll();
+
+		return list.stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList());
+	}
+
+}
