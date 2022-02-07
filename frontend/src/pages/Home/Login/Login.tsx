42293c0e1d6a925cce898b2dcb91480ce6c43186
Agny <agnyfonseca@outlook.com>
2022-02-07 19:45:17 +0100

    React Hook Form first test

--- frontend/src/pages/Home/Login/Login.tsx
+++ frontend/src/pages/Home/Login/Login.tsx
@@ -1,26 +1,46 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import Button from '../../../components/Button/Button';
 import './Login.css';
 
+type FormData = {
+    username: string;
+    password: string;
+}
+
 const Login = () => {
+
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (formData : FormData) => {
+        console.log(formData);
+    };
+
+
     return (
         <div className="login-card base-card">
             <h1>LOGIN</h1>
-            <form className="form-container">
+            <form onSubmit={handleSubmit(onSubmit)} className="form-container">
                 <div>
-                    <input 
+                    <input
+                        {...register("username")}
+                        type="text"
                         placeholder="Email"
+                        name="username"
                     />
                 </div>
                 <div>
                     <input
+                        {...register("password")}
+                        type="password"
                         placeholder="Senha"
+                        name="password"
                     />
                 </div>
                 <div>
-                    <Link to="/movies">
+                    {/* <Link to="/movies"> */}
                         <Button title="Fazer Login" />
-                    </Link>
+                    {/* </Link> */}
                 </div>
             </form>
         </div>
