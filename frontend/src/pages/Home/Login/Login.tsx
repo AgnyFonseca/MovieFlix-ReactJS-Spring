f1449681e9e46ade405a6301e7053a3c34c8cdd7
Agny <agnyfonseca@outlook.com>
2022-02-09 18:45:01 +0100

    Form validation

--- frontend/src/pages/Home/Login/Login.tsx
+++ frontend/src/pages/Home/Login/Login.tsx
@@ -1,5 +1,5 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
 import Button from '../../../components/Button/Button';
 import { requestBackendLogin } from '../../../util/requests';
 import './Login.css';
@@ -11,14 +11,18 @@ type FormData = {
 
 const Login = () => {
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
 
-    const onSubmit = (formData : FormData) => {
+    const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+
+    const onSubmit = (formData: FormData) => {
         requestBackendLogin(formData)
             .then(response => {
+                setHasError(false);
                 console.log('SUCESSO', formData);
             })
             .catch(error => {
+                setHasError(true);
                 console.log('ERRO', error);
             });
     };
@@ -27,26 +31,41 @@ const Login = () => {
     return (
         <div className="login-card base-card">
             <h1>LOGIN</h1>
+            {hasError && (
+                <div className="alert alert-danger">
+                    Erro ao tentar efetuar o login
+                </div>
+            )}
             <form onSubmit={handleSubmit(onSubmit)} className="form-container">
-                <div>
+                <div className="input-div">
                     <input
-                        {...register("username")}
+                        {...register('username', {
+                            required: 'Campo obrigatório',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'Email inválido'
+                            }
+                        })}
                         type="text"
                         placeholder="Email"
                         name="username"
                     />
+                    <div className="invalid-feedback d-block">{errors.username?.message}</div>
                 </div>
-                <div>
+                <div className="input-div">
                     <input
-                        {...register("password")}
+                        {...register('password', {
+                            required: 'Campo obrigatório'
+                        })}
                         type="password"
                         placeholder="Senha"
                         name="password"
                     />
+                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
                 </div>
                 <div>
                     {/* <Link to="/movies"> */}
-                        <Button title="Fazer Login" />
+                    <Button title="Fazer Login" />
                     {/* </Link> */}
                 </div>
             </form>
