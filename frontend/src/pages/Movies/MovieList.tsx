4fa04567ef6ea1a5539fd7a94ef3a02ab72729fa
Agny <agnyfonseca@outlook.com>
2022-02-18 14:35:25 +0100

    React select layout

--- frontend/src/pages/Movies/MovieList.tsx
+++ frontend/src/pages/Movies/MovieList.tsx
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Select from 'react-select';
 import MovieCard from '../../components/MovieCard/MovieCard';
 import { Movie } from '../../types/movie';
 import { SpringPage } from '../../types/spring';
@@ -9,6 +10,12 @@ import './MovieList.css';
 
 const MovieList = () => {
     const [page, setPage] = useState<SpringPage<Movie>>();
+
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+      ]
 
     useEffect(() => {
         const params: AxiosRequestConfig = {
@@ -27,6 +34,12 @@ const MovieList = () => {
 
     return (
         <div className="movie-list-container">
+            <div className="movie-list-select-box base-card">
+                <Select
+                    options={options}
+                    classNamePrefix="movie-list-select"
+                />
+            </div>
             <div className="row">
                 {page?.content.map((item) => (
                     <div className="col-sm-6 col-lg-4 col-xl-3" key={item.id}>
